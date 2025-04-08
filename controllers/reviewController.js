2cb16c8ce24778b1b48ad3ee25546793a4f70582
Frostic21 <145382864+Frostic21@users.noreply.github.com>
2025-04-08 08:43:20 -0700

    finish

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,4 +1,5 @@
 const {Product, Review} = require('../models')
+const res = require("express/lib/response");
 
 module.exports.renderAddReviewForm = async (req, res) => {
     const product = await Product.findByPk(
@@ -22,4 +23,39 @@ module.exports.addReview= async function(req, res) {
         product_id: req.params.productId
     });
     res.redirect(`/products/profile/${req.params.productId}`);
+}
+
+module.exports.renderEditReviewForm = async function (req, res) {
+    const review = await Review.findByPk(
+        req.params.id
+    );
+    res.render('reviews/edit', {review});
+}
+
+module.exports.updateReview = async function(req, res) {
+    const review = await Review.findByPk(
+        req.params.id
+    );
+        await Review.update({
+            customer_name: req.body.customer_name,
+            subject: req.body.subject,
+            rating: req.body.rating,
+            description: req.body.description,
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }
+    );
+    res.redirect(`/products/profile/${review.product_id}`);
+}
+
+module.exports.deleteReview= async function(req, res) {
+    const review = await Review.findByPk(req.params.id);
+    await Review.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/products/profile/${review.product_id}`);
 }
